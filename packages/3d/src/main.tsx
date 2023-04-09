fcb6ff9fb275df822e99e777d78658437e2695a6
Stafford Williams <stafford.williams@gmail.com>
2023-04-09 15:54:58 +1000

    background

--- packages/3d/src/main.tsx
+++ packages/3d/src/main.tsx
@@ -1,9 +1,10 @@
 import { Canvas } from "@react-three/fiber";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <Canvas className="canvas">
+  <Canvas className="canvas" flat linear>
     <App />
   </Canvas>
 );
